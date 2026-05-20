b52f5a2e43482bffb55c539f4cf412694310e0d1
dhanidds <tonodds@gmail.com>
2026-05-21 06:16:14 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,1 +1,29 @@
-# JAWAB DISINI!
+# ==========================================
+# FILE: jawaban_mahasiswa.py
+# NAMA: [Tulis Nama Anda di sini]
+# NIM: [Tulis NIM Anda di sini]
+# ==========================================
+
+def tambah(a, b):
+    """
+    Fungsi untuk menjumlahkan a dan b.
+    """
+    return a + b
+
+def kurang(a, b):
+    """
+    Fungsi untuk mengurangkan a dengan b.
+    """
+    return a - b
+
+def kali(a, b):
+    """
+    Fungsi untuk mengalikan a dan b.
+    """
+    return a * b
+
+def bagi(a, b):
+    """
+    Fungsi untuk membagi a dengan b.
+    """
+    return a / b
