f1f2c091a4c324041d4e40b7d22cc90d0146a186
sasikiranarzky-tech <sasikiranarzky@gmail.com>
2026-05-21 10:21:46 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -9,21 +9,19 @@ def tambah(a, b):
     Fungsi untuk menjumlahkan a dan b.
     """
     return a + b
-    print(tambah(10, 20))
-
+ 
 def kurang(a, b):
     """
     Fungsi untuk mengurangkan a dengan b.
     """
     return a - b 
-    print(kurang(15, 15))
-
+  
 def kali(a, b):
     """
     Fungsi untuk mengalikan a dan b.
     """
     return a * b 
-    print(kali(5, 8))
+   
 
 def bagi(a, b):
     """
@@ -32,10 +30,7 @@ def bagi(a, b):
     if b == 0 : 
         return "Error: Pembagian dengan nol tidak diperbolehkan"
     return a / b 
-    print(bagi(10, 2))
+   
 
 
-print(tambah(10, 20))
-print(kurang(15, 15))
-print(kali(5, 8))
-print(bagi(10, 2))
+
